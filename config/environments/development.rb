e805770edc71d749be50a82705628afbd46e7cf1
shonry271192 <shawn_dsouza@ashinfo.com>
2018-03-23 13:25:38 +0530

    Upload Image Successfully

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+Paperclip.options[:command_path] = 'C:\Program Files\GnuWin32\bin'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
